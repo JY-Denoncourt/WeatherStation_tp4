68b64c5a7548fcc6e4b3659303e2a7b1c6f6bdd2
JY-Denoncourt <masterlopette@hotmail.com>
2020-11-09 11:30:08 -0500

    JYD 1. Intégrer le projet  (copy projet x2)

---
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OpenWeatherAPITests
+{
+    public class OpenWeatherProcessorTests
+    {
+        #region (--) T01 Affiche exceptMess APIkey (valide / null)  --> GetOneCallAsync
+        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null
+
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+
+        }
+        #endregion
+
+
+        #region (--) T02 Affiche exceptMess APIkey (vide / null)
+        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
+
+        public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        { 
+
+
+        }
+        #endregion
+
+
+        #region (--) T03 Affiche Exception Message Client HTTP not initialize
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
+
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+
+        }
+        #endregion
+
+
+        #region (--) T04  Affiche Exception Message 
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
+
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+
+
+        }
+        #endregion
+    }
+}
