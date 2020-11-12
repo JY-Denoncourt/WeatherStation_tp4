97b646cf2c944495c43b5a3d64ea103af90ff335
JY-Denoncourt <masterlopette@hotmail.com>
2020-11-11 22:17:03 -0500

    JYD 03-Secret API key done

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WeatherApp
+{
+    static class AppConfiguration
+    {
+        static IConfiguration configuration;
+
+        static void initConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json",
+                optional: true,
+                reloadOnChange: true);
+
+            builder.AddUserSecrets<MainWindow>();
+
+            configuration = builder.Build();
+        }
+
+        static string getValue(string key)
+        {
+            //key possible { "Secret" 
+            //                   ==> "OWApiKey" }
+
+            if (configuration == null)
+                initConfig();
+                
+            return configuration.GetSection("Secrets")[key];
+        }
+
+
+    }
+}
